ac442318d0a6a79c5dedcaf5018364bfe5e55f32
Chibuzor Efedigbue <cefedigbuej@gmail.com>
2019-01-16 22:04:37 +0100

    add css precompilation.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("Welcome to Weather App");
+import './scss/index.scss';
+
+const content = document.querySelector('#content');
+
+const h1 = document.createElement('h1');
+
+h1.innerHTML = 'Welcome to Weather App!';
+
+content.appendChild(h1);
